569d4c8347b694ac35022b5c832be38d7a819950
Deivide Maciel <31144383+deivomaciel@users.noreply.github.com>
2024-01-20 18:31:14 +0000

    feat: Add rote the do intert a new content.

--- src/controllers/code.js
+++ src/controllers/code.js
@@ -1,4 +1,5 @@
 const { generateValue } = require('random-var')
+const redis = require('../services/redisServices')
 const { Router } = require("express")
 const router = Router()
 
@@ -7,9 +8,49 @@ const generateCode = () => { // Essa função deve ficar em um service??
     return code
 }
 
-router.get('/', async (req, res) => {
-    const code = generateCode()
-    res.status(200).send({ code })
+router.get('/getcode', async (req, res) => {
+    let code = generateCode()
+    let codeAlreadyExist = await redis.verifycode(code)
+
+    while(codeAlreadyExist) {
+        code = generateCode()
+        codeAlreadyExist = redis.verifycode(code)
+    }
+
+    try {
+        redis.insertCode(code.toString(), code)
+        res.status(200).send({ code })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ msg: 'Internal Server Error. Try later!' })
+    }
+})
+
+router.post('/addcontent', async(req, res) => {
+    let { code, content } = req.body
+
+    try {
+        if(code) {
+            const codeAlreadyExist = await redis.verifycode(code)
+            if(!codeAlreadyExist) return res.status(201).send({ msg: 'Invalid code!' })
+        } else return res.status(422).send({ msg: 'The code is mandatory!' })
+
+        const oldContent = await redis.getAllContent(code)
+        content = oldContent + ',' + content
+        redis.insertCode(code, content)
+        res.status(200).send({ msg: 'Content inserted successfully!' })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ msg: 'Internal Server Error. Try later!' })
+    }
+})
+
+router.post('/verifycode', async (req, res) => {
+    const code = req.body.code
+    if(!code || code.trim() == '') return res.status('422').send({ msg: 'The code is mandatory!' })
+    // Verificar no sistema de cache se o código existe
 })
 
 module.exports = router
---
+++ src/services/redisServices.js
@@ -0,0 +1,41 @@
+const { createClient } = require('@vercel/kv')
+
+const kv = createClient({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN
+})
+
+const insertCode = (code, value) => {
+    kv.set(code, value)
+    return 200
+}
+
+const verifycode = async key => {
+    const code = await kv.get(key)
+    if(!code) return false
+    return true
+}
+
+const deleteValue = value => {
+    kv.del(value)
+    return 200
+}
+
+const listAllKeys = async () => {
+    for await (const key of kv.scanIterator()) {
+        console.log(key)
+    }
+}
+
+const getAllContent = async code => {
+    const oldContent = await kv.get(code)
+    return oldContent
+}
+
+module.exports = {
+    insertCode,
+    verifycode,
+    deleteValue,
+    listAllKeys,
+    getAllContent
+}
