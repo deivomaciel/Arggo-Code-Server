e907e43b0d9ca21ad4bdf6e98ec4f95adca1bda8
Deivide Maciel <31144383+deivomaciel@users.noreply.github.com>
2024-01-22 00:20:17 +0000

    feat: Add getcontent rote

--- src/controllers/code.js
+++ src/controllers/code.js
@@ -12,10 +12,10 @@ router.get('/getcode', async (req, res) => {
     let code = generateCode()
     let codeAlreadyExist = await redis.verifycode(code)
 
-    // while(codeAlreadyExist) {
-    //     code = generateCode()
-    //     codeAlreadyExist = redis.verifycode(code)
-    // }
+    while(codeAlreadyExist) {
+        code = generateCode()
+        codeAlreadyExist = redis.verifycode(code)
+    }
 
     try {
         await redis.insertCode(code.toString(), code)
@@ -37,7 +37,7 @@ router.post('/addcontent', async(req, res) => {
         } else return res.status(422).send({ msg: 'The code is mandatory!' })
 
         const oldContent = await redis.getAllContent(code)
-        content = oldContent + ',' + content
+        content = oldContent + '---' + content
         await redis.insertCode(code, content)
         res.status(200).send({ msg: 'Content inserted successfully!' })
 
@@ -47,10 +47,23 @@ router.post('/addcontent', async(req, res) => {
     }
 })
 
-router.post('/verifycode', async (req, res) => {
-    const code = req.body.code
-    if(!code || code.trim() == '') return res.status('422').send({ msg: 'The code is mandatory!' })
-    // Verificar no sistema de cache se o código existe
+router.get('/getcontent', async (req, res) => {
+    const { code } = req.body
+
+    try {
+        if(code) {
+            const codeAlreadyExist = await redis.verifycode(code)
+            if(!codeAlreadyExist) return res.status(201).send({ msg: 'Invalid code!' })
+        } else return res.status(422).send({ msg: 'The code is mandatory!' })
+
+        const content = await redis.getAllContent(code)
+        if(!content) return res.status(404).send({ mgs: 'No content found!' })
+        res.status(200).send({ content })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ msg: 'Internal Server Error. Try later!' })
+    }
 })
 
 module.exports = router
